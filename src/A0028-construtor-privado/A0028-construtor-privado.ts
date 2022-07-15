c3711a47d051df84726720cb82b037a067de9f0e
Roger-Franco <rogerfrancogalt@gmail.com>
2022-07-15 08:52:18 -0400

    Construtor privado e Singleton

---
+++ src/A0028-construtor-privado/A0028-construtor-privado.ts
@@ -0,0 +1,35 @@
+// Singleton - GOF | Factory Method - GOF
+export class Database {
+  private static database: Database;
+  private constructor(
+    private host: string,
+    private user: string,
+    private password: string,
+  ) {}
+  connect(): void {
+    console.log(`Conectado: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(host: string, user: string, password: string): Database {
+    if (Database.database) {
+      console.log('Retornando instância já criada.');
+      return Database.database;
+    }
+    console.log('Criando nova instância.');
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
+}
+
+// const database = new Database('localhost', 'root', '12345678');
+// database.connect();
+const db1 = Database.getDatabase('localhost', 'root', '12345678');
+db1.connect();
+
+const db2 = Database.getDatabase('localhost', 'root', '12345678');
+db2.connect();
+
+const db3 = Database.getDatabase('localhost', 'root', '12345678');
+db3.connect();
+
+console.log(db1 === db2);
